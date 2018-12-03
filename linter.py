09f373126158c055d931e72300185906502c1504
Jordon Bedwell <jordon@envygeeks.io>
2018-12-03 14:30:58 -0600

    Lets see how this works, stay tuned for the rant.

---
+++ linter.py
@@ -0,0 +1,29 @@
+from SublimeLinter.lint import Linter
+
+
+class GoLint(Linter):
+    multiline = True
+    regex = r'(?:[^:]+):(?P<line>\d+):(?P<col>\d+)?:(?:(?P<warning>warning)|(?P<error>error)):\s*(?P<message>.*)'
+    cmd = 'gometalinter'
+
+    args = (
+      "--fast",
+      "--concurrency=12",
+      "--disable-all",
+      "--vendor",
+      "--tests",
+
+      "--enable=gosec",
+      "--enable=golint",
+      "--enable=errcheck",
+      "--enable=ineffassign",
+      "--enable=vetshadow",
+      "--enable=goconst",
+      "--enable=gocyclo",
+      "--enable=deadcode",
+      "--enable=safesql"
+    )
+
+    defaults = {
+        'selector': 'source.go'
+    }
