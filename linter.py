f8a02d175a960c8c8f52302aaeac7dd05fbeba9f
Jordon Bedwell <jordon@envygeeks.io>
2018-12-03 16:38:03 -0600

    Add ${args} so that we can make sure the file is last.

--- linter.py
+++ linter.py
@@ -6,7 +6,7 @@ class GoLint(Linter):
     default_type = WARNING
     error_stream = util.STREAM_STDOUT
     regex = r'(?:[^:]+):(?P<line>\d+):(?P<col>\d+)?(:(?:(?P<warning>warning)|(?P<error>error)))?:\s*(?P<message>.*)'
-    cmd = 'gometalinter --fast --concurrency=12 ${file}'
+    cmd = 'gometalinter --fast --concurrency=12 ${args} ${file}'
     defaults = {
         'selector': 'source.go'
     }
