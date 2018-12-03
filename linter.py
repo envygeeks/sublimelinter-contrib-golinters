7291aebe1d5bb2636cbfc25c149188c5007418cc
Jordon Bedwell <jordon@envygeeks.io>
2018-12-03 16:37:17 -0600

    Make sure we do file only.

--- linter.py
+++ linter.py
@@ -2,6 +2,7 @@ from SublimeLinter.lint import util, Linter, WARNING
 
 
 class GoLint(Linter):
+    tempfile_suffix = "go"
     default_type = WARNING
     error_stream = util.STREAM_STDOUT
     regex = r'(?:[^:]+):(?P<line>\d+):(?P<col>\d+)?(:(?:(?P<warning>warning)|(?P<error>error)))?:\s*(?P<message>.*)'
