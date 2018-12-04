278c3118c0d289553e55b43e49e5b9c234dddbdb
Jordon Bedwell <jordon@envygeeks.io>
2018-12-03 21:38:13 -0600

    Lets try this\!

--- linter.py
+++ linter.py
@@ -7,25 +7,19 @@ class GoLint(Linter):
     default_type = WARNING
     defaults = { 'selector': 'source.go' }
     regex = r'(?:[^:]+):(?P<line>\d+):(?P<col>\d+)?(:(?:(?P<warning>warning)|(?P<error>error)))?:\s*(?P<message>.*)'
-    args = ("--fast", "${args}", "${file}")
     error_stream = util.STREAM_STDOUT
+    default_args = ("--fast")
 
     def cmd(self):
       """Gives back the command with a relative path."""
       f, e = path.basename(self.filename), self.which("gometalinter")
       if e is not None and f is not "":
-        # We'll still get a dep and have to strip it out 🤷‍♂️
-        return (e,) + self.args + ("${args}", f, "${file}")
+        return (e,) + self.default_args + (
+          "${args}", f, "${file}")
       return None
 
     def finalize_cmd(self, cmd, context, at_value='', auto_append=False):
-      c = super().finalize_cmd(cmd, context, at_value, auto_append)
-      print("golinters: finalize_cmd original got {}".format(c))
-      f = self.filename
+      c, f = super().finalize_cmd(cmd, context, at_value, auto_append), self.filename
       if f is not "":
-        i = c.index(f)
-        print("golinters: index at {}".format(i))
-        del c[i]
-
-      print("golinters: returning {}".format(c))
+        del c[c.index(f)]
       return c
