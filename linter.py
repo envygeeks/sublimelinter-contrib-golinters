45fe152c63171f2da5c2b78309faf8a526016419
Jordon Bedwell <jordon@envygeeks.io>
2018-12-03 19:01:46 -0600

    Try to fix linting.

--- linter.py
+++ linter.py
@@ -1,12 +1,24 @@
 from SublimeLinter.lint import util, Linter, WARNING
+from os import path
 
 
 class GoLint(Linter):
-    tempfile_suffix = "go"
+    tempfile_suffix = "-"
     default_type = WARNING
     error_stream = util.STREAM_STDOUT
     regex = r'(?:[^:]+):(?P<line>\d+):(?P<col>\d+)?(:(?:(?P<warning>warning)|(?P<error>error)))?:\s*(?P<message>.*)'
-    cmd = 'gometalinter --fast --concurrency=12 ${args} ${file}'
     defaults = {
         'selector': 'source.go'
     }
+
+    def cmd(self):
+      """Gives back the command with a relative path."""
+      f, e = path.basename(self.filepath), self.which("gometalinter")
+      if e is not None:
+        return (
+          e,
+          "--fast", "--concurrency=12",
+          "${args}", f
+        )
+
+      return None
