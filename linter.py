5ef28b7c6e51b1b1b3266d2fda7b83eea966bc73
Jordon Bedwell <jordon@envygeeks.io>
2018-12-03 21:08:53 -0600

    We might have to use finalize_cmd()

--- linter.py
+++ linter.py
@@ -7,19 +7,20 @@ class GoLint(Linter):
     default_type = WARNING
     defaults = { 'selector': 'source.go' }
     regex = r'(?:[^:]+):(?P<line>\d+):(?P<col>\d+)?(:(?:(?P<warning>warning)|(?P<error>error)))?:\s*(?P<message>.*)'
-    args = ("--fast", "--concurrency=12", "${args}")
+    args = ("--fast", "${args}", "${file}")
     error_stream = util.STREAM_STDOUT
 
     def cmd(self):
       """Gives back the command with a relative path."""
       f, e = path.basename(self.filename), self.which("gometalinter")
       if e is not None and f is not "":
-        return (e,) + self.args
+        # We'll still get a dep and have to strip it out 🤷‍♂️
+        return (e,) + self.args + ("${args}", f, "${file}")
       return None
 
-    def get_cmd(self):
-      args = super().get_cmd()
-      print('golinters: {}'.format(args))
-      if args[-1] == self.filename:
-        del args[-1]
-      return args
+    def finalize_cmd(self, cmd, context, at_value='', auto_append=False):
+      c = super().finalize_cmd(cmd, context, at_value, auto_append)
+      f = self.filename
+      if f is not "":
+        del c[c.index(f)]
+      return c
