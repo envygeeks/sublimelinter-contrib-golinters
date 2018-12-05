648d684fc237fdf382ed93d3c6eb1acaede06d9e
Jordon Bedwell <jordon@envygeeks.io>
2018-12-05 03:19:43 -0600

    Debugging Sublime is terrible.

--- linter.py
+++ linter.py
@@ -16,7 +16,7 @@ class GoLint(Linter):
       r = self.relative_path
       e = self.which("gometalinter")
       if e is not None and r is not "":
-        return (e, "${args}",)
+        return (e, "${args}", "${file}")
       return None
 
     @property
