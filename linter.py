5c02944321283e5f9bd107df864724d1693ea98d
Jordon Bedwell <jordon@envygeeks.io>
2018-12-05 03:49:33 -0600

    We don't want the include anymore.

--- linter.py
+++ linter.py
@@ -22,7 +22,7 @@ class GoLint(Linter):
       r = self.relative_path
       e = self.which("gometalinter")
       if e is not None and r is not "":
-        return (e, "--include", "\\A{}".format(r), "${args}", ".")
+        return (e, "${args}", ".")
       return None
 
     @property
